fc8087a4b350d47e2da52f4399f0d283c15e8b1c
Yuan <lugir.com@gmail.com>
2023-01-14 22:43:31 +0800

    Use 'let' and 'const' replace 'var' keyword

--- js/actions.js
+++ js/actions.js
@@ -1,55 +1,57 @@
 (function (Drupal) {
   document.addEventListener('DOMContentLoaded', function () {
     // Initial checkpoint.
-    var captchaVerified = false;
+    let captchaVerified = false;
 
-    var button = document.getElementById('tencentcloud-captcha');
+    let captchaBtn = document.getElementById('tencentcloud-captcha');
     // Hide captcha container or button when there is a submit button (which will be used to trigger the captcha button).
     if (document.getElementById('edit-submit')) {
-      var captchaContainer = document.querySelector('details#captcha');
+      let captchaContainer = document.querySelector('details#captcha');
       // Hide captcha container if there is one.
       if (captchaContainer) {
         captchaContainer.style.display = 'none';
       }
       // Hide captcha button if on container.
       else {
-        button.style.display = 'none';
+        captchaBtn.style.display = 'none';
       }
     }
 
-    var appId = button.getAttribute('data-app-id');
-    button.setAttribute('type', 'button');
+    let appId = captchaBtn.getAttribute('data-app-id');
+    captchaBtn.setAttribute('type', 'button');
 
-    var tencentCloudCaptcha = new TencentCaptcha(appId, function(res) {
+    let tencentCloudCaptcha = new TencentCaptcha(appId, function(res) {
       if (res.ret !== 0){
         return ;
       }
       document.getElementById('captcha-ticket').value = res.ticket;
       document.getElementById('captcha-randstr').value = res.randstr;
-      button.value = Drupal.t('Passed');
-      button.setAttribute('disabled','disabled');
+      captchaBtn.value = Drupal.t('Passed');
+      captchaBtn.setAttribute('disabled','disabled');
 
       // Trigger submit.
       captchaVerified = true;
-      button.closest('form').querySelector('#edit-submit').click();
+      captchaBtn.closest('form').querySelector('#edit-submit').click();
     });
 
 
-    button.addEventListener('click', function (event) {
+    captchaBtn.addEventListener('click', function (event) {
       tencentCloudCaptcha.show();
     });
 
     // Intercept submit action if the form has a unverified tencentcloud captcha.
-    document.addEventListener('click', function (event) {
-
-      // If the clicked element doesn't have the right selector, bail
-      //if (!event.target.matches('#edit-submit')) return;
-      formIsValid = event.target.closest('form').checkValidity();
-      var button = this.getElementById('edit-submit');
-      if (button && formIsValid && !captchaVerified) {
+    let submitBtn = document.getElementById('edit-submit');
+    submitBtn.addEventListener('click', function (event) {
+      console.log('click submit');
+      let formIsValid = event.target.closest('form').checkValidity();
+      console.log('formIsValid: ', formIsValid);
+      console.log('captchaVerified: ', captchaVerified);
+      // Prevent submit and call captcha when form is valid BUT the captcha is not verified.
+      if (formIsValid && !captchaVerified) {
         event.preventDefault();
-        button.click();
+        captchaBtn.click();
       }
+      // else submit form as normal.
     }, false);
 
   }, false);
--- js/prepare.js
+++ js/prepare.js
@@ -2,18 +2,18 @@
   document.addEventListener('DOMContentLoaded', function () {
     // Disable or hide submit button until captcha is ready, actions.js will re-enable it.
     // console.log('tencentcloud_captcha: Disable submit button');
-    var btnSubmit = document.getElementById('tencentcloud-captcha').closest('form').querySelector('#edit-submit');
+    let btnSubmit = document.getElementById('tencentcloud-captcha').closest('form').querySelector('#edit-submit');
     if (btnSubmit) {
       btnSubmit.setAttribute('disabled', 'disabled')
     }
   }, false);
 
-  var checkReadyState = setInterval(function() {
+  let checkReadyState = setInterval(function() {
     if (document.readyState === "complete") {
         clearInterval(checkReadyState);
         // Enable/show submit button after page is ready.
         // console.log('tencentcloud_captcha: Enable submit button');
-        var btnSubmit = document.getElementById('tencentcloud-captcha').closest('form').querySelector('#edit-submit');
+        let btnSubmit = document.getElementById('tencentcloud-captcha').closest('form').querySelector('#edit-submit');
         if (btnSubmit) {
           btnSubmit.removeAttribute('disabled')
         }
